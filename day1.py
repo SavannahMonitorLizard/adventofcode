d528da595443aed952ef9df60783d9b9be4bf7ab
TymurTkachenko <tymur.tk@gmail.com>
2020-12-01 13:12:39 -0500

    day 1

--- day1.py
+++ day1.py
@@ -2,9 +2,22 @@ nums = open("inputs\\day1.txt").read().split("\n")
 nums.remove('')
 for i in range(0, len(nums)): 
     nums[i] = int(nums[i])
+
+part1 = 0
+for i in range(len(nums)):
+    for j in range(len(nums)):
+        if nums[i] + nums[j] == 2020:
+            part1 = nums[i] * nums[j]
+            break
+
+print(f"Part 1: {part1}")
+
+part2 = 0
 for i in range(len(nums)):
     for j in range(len(nums)):
         for k in range(len(nums)):
             if nums[i] + nums[j] + nums[k] == 2020:
-                print(nums[i] * nums[j] * nums[k])
+                part2 = nums[i] * nums[j] * nums[k]
                 break
+
+print(f"Part 2: {part2}")
