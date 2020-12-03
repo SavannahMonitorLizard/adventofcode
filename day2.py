02083a4053d48d124b3a096988f84a123915737d
TymurTkachenko <tymur.tk@gmail.com>
2020-12-03 09:56:40 -0500

    day 2

--- day2.py
+++ day2.py
@@ -4,43 +4,29 @@ passwords.remove('')
 total = 0
 
 for i in range(len(passwords)):
-    temp = passwords[i].split(": ")
-    temp2 = temp[0].split(" ")
-    temp3 = temp2[0].split("-")
-    temp3[0] = int(temp3[0])
-    temp3[1] = int(temp3[1])
+    nums, password = passwords[i].split(": ")
+    nums, letter = nums.split(" ")
+    minV, maxV = [int(i) for i in nums.split('-')]
     count = 0
-
-    password = temp[1]
-    letter = temp2[1]
-    index1 = temp3[0]
-    index2 = temp3[1]
 
     for j in password:
         if j == letter:
             count += 1
 
-    if count >= index1 and count <= index2:
+    if minV <= count <= maxV:
         total += 1
 
-print(f"part 1: {total}")
+print(f"Part 1: {total}")
 
 total = 0
 
 for i in range(len(passwords)):
-    temp = passwords[i].split(": ")
-    temp2 = temp[0].split(" ")
-    temp3 = temp2[0].split("-")
-    temp3[0] = int(temp3[0])
-    temp3[1] = int(temp3[1])
-
-    password = temp[1]
-    letter = temp2[1]
-    index1 = temp3[0]
-    index2 = temp3[1]
+    nums, password = passwords[i].split(": ")
+    nums, letter = nums.split(" ")
+    index1, index2 = [int(i) for i in nums.split('-')]
 
     if password[index1 - 1] == letter or password[index2 - 1] == letter:
         if not (password[index1 - 1] == letter and password[index2 - 1] == letter):
             total += 1
 
-print(f"part 2: {total}")
+print(f"Part 2: {total}")
