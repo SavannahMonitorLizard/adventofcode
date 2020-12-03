ceed35769a27a46624add523ca27dce80d940eb8
TymurTkachenko <tymur.tk@gmail.com>
2020-12-03 01:03:54 -0500

    day 3

--- day3.py
+++ day3.py
@@ -15,58 +15,30 @@ for i in range(len(trees)):
 
 print(f"Part 1: {count}")
 
-point = [0, 0]
-r1d1 = 0
-r5d1 = 0
-r7d1 = 0
-r1d2 = 0
+slopes = [
+    (1, 1),
+    (3, 1),
+    (5, 1),
+    (7, 1),
+    (1, 2)
+]
 
-for i in range(len(trees)):
-    point[0] = i + 1
-    point[1] += 1
+total = 1
 
-    try:
-        if trees[i + 1][point[1] % 31] == "#":
-            r1d1 += 1
-    except IndexError:
-        break
+for s in slopes:
+    point = [0, 0]
+    count = 0
+    for i in range(len(trees)):
+        dx, dy = s
+        point[0] += dy
+        point[1] += dx
 
-point = [0, 0]
+        try:
+            if trees[point[0]][point[1] % 31] == "#":
+                count += 1
+        except IndexError:
+            break
 
-for i in range(len(trees)):
-    point[0] = i + 1
-    point[1] += 5
+    total *= count
 
-    try:
-        if trees[i + 1][point[1] % 31] == "#":
-            r5d1 += 1
-    except IndexError:
-        break
-
-point = [0, 0]
-
-for i in range(len(trees)):
-    point[0] = i + 1
-    point[1] += 7
-
-    try:
-        if trees[i + 1][point[1] % 31] == "#":
-            r7d1 += 1
-    except IndexError:
-        break
-
-point = [0, 0]
-counter = 0
-
-for i in range(len(trees)):
-    counter += 2
-    point[0] = counter
-    point[1] += 1
-
-    try:
-        if trees[counter][point[1] % 31] == "#":
-            r1d2 += 1
-    except IndexError:
-        break
-
-print(f"Part 2: {count * r1d1 * r5d1 * r7d1 * r1d2}")
+print(f"Part 2: {total}")
