89cb491b8ec138393dcc57e539ed0151b457b19c
TymurTkachenko <tymur.tk@gmail.com>
2020-12-06 02:03:13 -0500

    using more than 2 lines wack

--- day6.py
+++ day6.py
@@ -1,22 +1,3 @@
 answers = open('inputs\\day6.txt', 'r').read().split("\n\n")
 
-total = 0
-
-for answer in answers:
-    counted = []
-    count = 0
-    for letter in answer:
-        if letter not in counted and letter != "\n":
-            count += 1
-            counted += [letter]
-
-    total += count
-
-print(f"Part 1: {total}")
-
-total = 0
-
-for answer in answers:
-    total += len(list(set.intersection(*map(set, answer.split("\n")))))
-
-print(f"Part 2: {total}")
+print("Part 1: " + str(sum([len(list(set.union(*map(set, answer.split("\n"))))) for answer in answers])) + "\nPart 2: " + str(sum([len(list(set.intersection(*map(set, answer.split("\n"))))) for answer in answers])))
