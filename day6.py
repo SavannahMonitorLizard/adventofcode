7ec434bffa85c9525d8d61e04ae826119b3caad2
TymurTkachenko <tymur.tk@gmail.com>
2020-12-06 02:08:40 -0500

    one 1 line required

--- day6.py
+++ day6.py
@@ -1,3 +1,1 @@
-answers = open('inputs\\day6.txt', 'r').read().split("\n\n")
-
-print("Part 1: " + str(sum([len(list(set.union(*map(set, answer.split("\n"))))) for answer in answers])) + "\nPart 2: " + str(sum([len(list(set.intersection(*map(set, answer.split("\n"))))) for answer in answers])))
+print("Part 1: " + str(sum([len(list(set.union(*map(set, answer.split("\n"))))) for answer in open('inputs\\day6.txt', 'r').read().split("\n\n")])) + "\nPart 2: " + str(sum([len(list(set.intersection(*map(set, answer.split("\n"))))) for answer in open('inputs\\day6.txt', 'r').read().split("\n\n")])))
