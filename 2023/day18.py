2c1b4f5256daa2c696a12be10a333d6f17c7e02a
Tymur Tkachenko <tymur.tk@gmail.com>
2023-12-19 05:44:04 -0500

    day 18

--- 2023/day18.py
+++ 2023/day18.py
@@ -8,10 +8,60 @@ def main():
     p1(lines)
     p2(lines)
 
-def p1(grid):
-    pass
+def p1(lines):
+    def polygon_area(vertices):
+        n = len(vertices)
+        area = 0.5 * abs(sum(vertices[i][0] * vertices[(i + 1) % n][1] - vertices[(i + 1) % n][0] * vertices[i][1] for i in range(n)))
+        return area
+    
+    pos = (0, 0)
+    vertices = []
+    boundary = 0
+    for line in lines:
+        direction, distance, _ = line.split(" ")
+        distance = int(distance)
+        boundary += distance
 
-def p2(grid):
-    pass
+        if direction == 'R':
+            pos = (pos[0], pos[1] + distance)
+        elif direction == 'L':
+            pos = (pos[0], pos[1] - distance)
+        elif direction == 'U':
+            pos = (pos[0] - distance, pos[1])
+        elif direction == 'D':
+            pos = (pos[0] + distance, pos[1])
+
+        vertices.append(pos)
+
+    print(int(polygon_area(vertices) - (boundary / 2) + 1 + boundary))
+
+def p2(lines):
+    def polygon_area(vertices):
+        n = len(vertices)
+        area = 0.5 * abs(sum(vertices[i][0] * vertices[(i + 1) % n][1] - vertices[(i + 1) % n][0] * vertices[i][1] for i in range(n)))
+        return area
+    
+    pos = (0, 0)
+    vertices = []
+    boundary = 0
+    for line in lines:
+        _, _, code = line.split(" ")
+        direction = code[-2]
+        code = code[2:-2]
+        distance = int(code, 16)
+        boundary += distance
+
+        if direction == '0':
+            pos = (pos[0], pos[1] + distance)
+        elif direction == '2':
+            pos = (pos[0], pos[1] - distance)
+        elif direction == '3':
+            pos = (pos[0] - distance, pos[1])
+        elif direction == '1':
+            pos = (pos[0] + distance, pos[1])
+
+        vertices.append(pos)
+
+    print(int(polygon_area(vertices) - (boundary / 2) + 1 + boundary))
 
 main()
