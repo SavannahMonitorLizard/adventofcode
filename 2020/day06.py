f5c8a394bfecc6bdadf0925da4776544dca72ac5
Tymur Tkachenko <tymur.tk@gmail.com>
2023-12-24 04:00:50 -0500

    day20

--- 2020/day06.py
+++
@@ -1,1 +0,0 @@
-print("Part 1: " + str(sum([len(list(set.union(*map(set, answer.split("\n"))))) for answer in open('inputs\\day6.txt', 'r').read().split("\n\n")])) + "\nPart 2: " + str(sum([len(list(set.intersection(*map(set, answer.split("\n"))))) for answer in open('inputs\\day6.txt', 'r').read().split("\n\n")])))
